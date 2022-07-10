13eac35a690032721dac764bc25b45437946090d
Jules Pascot <jules.pascot@gmail.com>
2022-07-10 18:58:19 +0200

    chore: Addition of webpack and babel to the project

---
+++ src/pages/download/download.js
@@ -0,0 +1,4 @@
+import "./../../assets/scss/main.scss";
+import "./download.scss";
+
+console.log("Download");
---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: path.join(__dirname, "src/index.js"),
+    download: path.join(__dirname, "src/pages/download/download.js"),
+    login: path.join(__dirname, "src/pages/login/login.js"),
+    premium: path.join(__dirname, "src/pages/premium/premium.js"),
+    signin: path.join(__dirname, "src/pages/signin/signin.js"),
+    support: path.join(__dirname, "src/pages/support/support.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.scss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "./src/assets/images/*",
+          to: "assets/images/[name][ext]",
+        },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: path.join(__dirname, "src/index.html"),
+      chunks: ["main"],
+    }),
+    new HtmlWebpackPlugin({
+      filename: "download.html",
+      template: path.join(__dirname, "src/pages/download/download.html"),
+      chunks: ["download"],
+    }),
+    new HtmlWebpackPlugin({
+      filename: "login.html",
+      template: path.join(__dirname, "src/pages/login/login.html"),
+      chunks: ["login"],
+    }),
+    new HtmlWebpackPlugin({
+      filename: "premium.html",
+      template: path.join(__dirname, "src/pages/premium/premium.html"),
+      chunks: ["premium"],
+    }),
+    new HtmlWebpackPlugin({
+      filename: "signin.html",
+      template: path.join(__dirname, "src/pages/signin/signin.html"),
+      chunks: ["signin"],
+    }),
+    new HtmlWebpackPlugin({
+      filename: "support.html",
+      template: path.join(__dirname, "src/pages/support/support.html"),
+      chunks: ["support"],
+    }),
+  ],
+  devtool: "source-map",
+  mode: "development",
+  devServer: {
+    static: path.resolve(__dirname, "./dist"),
+    open: true,
+    port: 4000,
+  },
+};
